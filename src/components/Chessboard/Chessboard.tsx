965890ccdd9bd0f91a8dd98cf7c03fa16a8c06d5
prrockzed <prrockzed@pm.me>
2023-01-23 13:59:10 +0530

    The chess pieces are moveable, but out of control

--- src/components/Chessboard/Chessboard.tsx
+++ src/components/Chessboard/Chessboard.tsx
@@ -1,3 +1,4 @@
+import React from 'react';
 import Tile from '../Tile/Tile'
 import './Chessboard.css';
 
@@ -48,6 +49,41 @@ for (let p = 0; p < 2; p++) {
 }
 
 
+let activePiece: HTMLElement | null = null;
+
+function grabPiece(e: React.MouseEvent) {
+  const element = e.target as HTMLElement;
+
+  if (element.classList.contains("chess-piece")) {
+    const x = e.clientX - 50;
+    const y = e.clientY - 50;
+
+    element.style.position = "absolute";
+    element.style.left = `${x}px`;
+    element.style.top = `${y}px`;
+
+    activePiece = element;
+  }
+}
+
+function movePiece(e: React.MouseEvent) {
+  if (activePiece) {
+    const x = e.clientX - 50;
+    const y = e.clientY - 50;
+
+    activePiece.style.position = "absolute";
+    activePiece.style.left = `${x}px`;
+    activePiece.style.top = `${y}px`;
+  }
+}
+
+function dropPiece(e: React.MouseEvent) {
+  if (activePiece) {
+    activePiece = null;
+  }
+}
+
+
 export default function Chessboard() {
 
   let board = [];
@@ -63,9 +99,18 @@ export default function Chessboard() {
         }
       })
 
-      board.push(<Tile image={image} number={number} />)
+      board.push(<Tile key={`${j},${i}`} image={image} number={number} />)
     }
   }
 
-  return <div id="chessboard">{board}</div>
+  return (
+    <div
+      onMouseMove={e => movePiece(e)}
+      onMouseDown={e => grabPiece(e)}
+      onMouseUp={e => dropPiece(e)}
+      id="chessboard"
+    >
+      {board}
+    </div>
+  )
 }
